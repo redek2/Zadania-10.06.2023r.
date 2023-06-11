cacf7f2be05563f10865430ee6fa9a637a94687f
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-11 15:54:27 +0200

    Add files via upload

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,30 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int a, n;
+
+        Console.WriteLine("Program wypisuje n pierwszych wyrazów ciągu Fibonacciego");
+        Console.WriteLine("Podaj n:");
+        n = int.Parse(Console.ReadLine());
+        Console.WriteLine("\n");
+
+        for(int i = 1; i <= n; i++)
+        {
+            a = CiagFibonacciego(i);
+            Console.WriteLine("" + a);
+        }
+    }
+
+    static int CiagFibonacciego(int n)
+    {
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return CiagFibonacciego(n - 1) + CiagFibonacciego(n - 2);
+        }
+    }
+}
