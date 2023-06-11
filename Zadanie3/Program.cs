1995539f1a2796c35cddfb9bf5819b5ffbb09a46
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-11 19:47:42 +0200

    Add files via upload

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,45 @@
+﻿public class MyProgram
+{
+    public static double Silnia(double n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Silnia(n - 1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        double m, n, k, a, b;
+
+        Console.WriteLine("Program wylicza równanie m=(n!-k!)/k!, dla naturalnych n, k >= 5");
+        Console.WriteLine("Podaj n:");
+        n = double.Parse(Console.ReadLine());
+
+        while (n < 5)
+        {
+            Console.WriteLine("Proszę podać liczbę większą lub równą 5");
+            n = double.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Podaj k:");
+        k = double.Parse(Console.ReadLine());
+
+        while (k < 5)
+        {
+            Console.WriteLine("Proszę podać liczbę większą lub równą 5");
+            k = double.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nm=(" + n + "!-" + k + "!)/" + k + "!");
+        a = Silnia(n);
+        b = Silnia(k);
+        Console.WriteLine("\nm=("+a+"-"+b+")/"+b);
+        m = (a - b) / b;
+        Console.WriteLine("\nm=" + m);
+    }
+}
