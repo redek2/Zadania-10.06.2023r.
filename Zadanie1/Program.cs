296a00904bda6ff8078b6ffac65bc520ecff4b6c
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-11 15:00:55 +0200

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        c = 1;
+
+        Console.WriteLine("Podaj naturalną podstawę potęgi:");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("\nPodaj naturalny wykładnik potęgi");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("\n");
+
+        for (int i = 1; i <= b; i++)
+        {
+            c *= a;
+        }
+
+        Console.WriteLine("Wynik " + a + " do potęgi " + b + " jest równy " + c);
+    }
+}
