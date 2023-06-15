ea98d3218cf5b232de7f1fcbcc33edbced034878
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-15 17:15:18 +0200

    Add files via upload

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,25 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int n, x;
+        double e;
+        e = 0;
+        long Silnia(int n)
+        {
+            return n == 0 ? 1 : n * Silnia(n - 1);
+        }
+
+        Console.WriteLine("Suma n pierwszych wyrazów funkcji e^x\n");
+        Console.WriteLine("Podaj n: ");
+        n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj x: ");
+        x = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            e += Math.Pow(x, i) / Silnia(i);
+        }
+        Console.WriteLine("Suma {0} pierwszych wyrazów funkcji e^x wynosi: {1}", n, e);
+    }
+}
